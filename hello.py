a8516d93f1fde7def1680e4e393d2d018c9f52cf
MuhammadSyahrizalHidayat <muhammadsyahrizalhidayat_2226250086@mhs.mdp.ac.id>
2025-11-12 14:21:12 +0700

    commit 3

--- hello.py
+++ hello.py
@@ -5,7 +5,8 @@ app = Flask(__name__, template_folder='views')
 # define route for the root URL
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    title = "Home Page"
+    return render_template('index.html', title=title)
 # define route for a template rendering
 @app.route('/about')
 def about():
@@ -15,7 +16,12 @@ def about():
 @app.route('/contact')
 def contact():
     title = "Contact Page"
-    return render_template('contact.html', title=title) 
+    return render_template('contact.html', title=title)
+
+@app.route('/pmb')
+def pmb():
+    title = "Penerimaan Mahasiswa Baru"
+    return render_template('pmb.html', title=title)
 # run the app
 if __name__ == '__main__':
     app.run(debug=True)
